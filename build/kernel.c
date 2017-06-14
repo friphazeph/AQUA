1f225cdbe017accf9a47b8c8662db15a5f732759
no92 <no92.mail@gmail.com>
2017-06-14 12:37:28 +0200

    reorganize the directory structure

--- build/kernel.c
+++ build/kernel.c
@@ -1,27 +1,27 @@
 /*
  * kernel.c
- * 
+ *
  * Copyright 2017 Obiwac
- * 
+ *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
  * MA 02110-1301, USA.
- * 
- * 
+ *
+ *
  */
 
-
+/* TODO: move this to src/ */
 #include "../include/multiboot.h"
 
 #include "../include/types.h"
@@ -43,23 +43,23 @@ extern void call_constructors(void) {
 	constructor* i;
 	for (i = &start_ctors; i != &end_ctors; i++) {
 		(*i)();
-		
+
 	}
-	
+
 }
 
 extern void main(unsigned long magic, unsigned long addr) {
 	uint8 terminal_colour = 0x02;
-	
+
 	/*if (drive) {
 		print("Setting up Advanced Technoligy Attachment (Primary Master) enviroment.\n", terminal_colour);
 		ata_setup(1, 0x1F0);
-		
+
 		print("Identifying Advanced Technoligy Attachment (Primary Master): \n", terminal_colour);
 		identify();
-		
+
 	}*/
-	
+
 	OS_run(\
 		magic, \
 		addr, \
@@ -88,5 +88,5 @@ extern void main(unsigned long magic, unsigned long addr) {
 		icon_load \
 		\
 	);
-	
+
 }
